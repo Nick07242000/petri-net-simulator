410361f789031f55bf846f20fa1d5d32ca28856b
Nick07242000 <nicohidalgo2000@gmail.com>
2023-12-18 02:14:03 -0300

    [ADD] P Invariant check

--- src/main/java/org/nnf/pns/model/PetriNet.java
+++ src/main/java/org/nnf/pns/model/PetriNet.java
@@ -8,9 +8,14 @@ import org.apache.log4j.Logger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 import static java.lang.Boolean.TRUE;
 import static java.util.Arrays.*;
+import static java.util.stream.IntStream.of;
 import static org.nnf.pns.util.Constants.*;
 import static org.nnf.pns.util.Net.createSequence;
 import static org.nnf.pns.util.Net.stringifyArray;
@@ -27,7 +32,7 @@ public class PetriNet {
         log.debug("Firing transitions: " + Arrays.toString(transitions));
 
         if (!areSensitized(transitions)) {
-            log.debug("Transitions not sensitized: fire cannot be performed");
+            log.error("Transitions not sensitized: fire cannot be performed");
             return;
         }
 
@@ -40,6 +45,9 @@ public class PetriNet {
                 .transpose();
 
         log.debug("New Marking: " + stringifyArray(currentMarking.getRow(0)));
+
+        if (!arePInvariantValid())
+            log.error("Fire error: P Invariants violated");
     }
 
     public boolean areSensitized(int... transitions) {
@@ -49,19 +57,18 @@ public class PetriNet {
     }
 
     public boolean isSensitized(int transition) {
-        for (int i = 0; i < PLACES_COUNT; i++) {
+        for (int i = 0; i < PLACES_COUNT; i++)
             if (incidenceMatrix.getEntry(i, transition) == -1 && currentMarking.getEntry(0, i) < 1)
                 return false;
-        }
+
         return true;
     }
 
     public int[] getSensitizedTransitions() {
         int[] sensitizedTransitions = new int[TRANSITIONS_COUNT];
 
-        for (int i = 0; i < TRANSITIONS_COUNT; i++) {
+        for (int i = 0; i < TRANSITIONS_COUNT; i++)
             sensitizedTransitions[i] = isSensitized(i) ? 1 : 0;
-        }
 
         log.debug("Sensitized Transitions: " + stringifyArray(sensitizedTransitions));
 
@@ -72,11 +79,9 @@ public class PetriNet {
         int[] sensitizedTransitions = getSensitizedTransitions();
         List<Integer> indexTransitions = new ArrayList<>();
 
-        for (int i = 1; i < TRANSITIONS_COUNT; i++) {
-            if (sensitizedTransitions[i] == 1) {
+        for (int i = 1; i < TRANSITIONS_COUNT; i++)
+            if (sensitizedTransitions[i] == 1)
                 indexTransitions.add(i);
-            }
-        }
 
         return indexTransitions;
     }
@@ -88,5 +93,22 @@ public class PetriNet {
 
     public boolean hasInitialState() {
         return deepEquals(currentMarking.getData(), INITIAL_MARKING);
+    }
+
+    public boolean arePInvariantValid() {
+        BiPredicate<int[], Integer> evaluator = (marks, sum) -> of(marks)
+                .map(m -> (int) currentMarking.getEntry(0,m))
+                .sum() == sum;
+
+        boolean ec1 = evaluator.test(new int[] {1,3},1);
+        boolean ec2 = evaluator.test(new int[] {2,5},1);
+        boolean ec4 = evaluator.test(new int[] {7,9},1);
+        boolean ec5 = evaluator.test(new int[] {8,11},1);
+        boolean ec7 = evaluator.test(new int[] {17,18},1);
+        boolean ec3 = evaluator.test(new int[] {13,14,15},1);
+        boolean ec6 = evaluator.test(new int[] {9,10,11},2);
+        boolean ec8 = evaluator.test(new int[] {4,5,17},3);
+
+        return ec1 && ec2 && ec3 && ec4 && ec5 && ec6 && ec7 && ec8;
     }
 }
